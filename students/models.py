2a8259248b832cad8fbe67da704f426bea33e18d
Vitor Dallabona <59742775+VitorDallabona@users.noreply.github.com>
2026-04-22 20:33:58 -0300

    commit inicial com a estrutura base do projeto

---
+++ students/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+class Student(models.Model):
+    """ 
+        Guarda as informacoes sobre o aluno
+        
+        - Nome: nome completo do aluno
+        - registration_id: matricula do aluno
+        - face_encoding: array do face_recognition
+        - created_at: data de cadastro do aluno
+    """
+    
+    name = models.CharField(max_length=200)
+    registration_id = models.CharField(max_length=20, unique=True)
+    face_encoding = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name} ({self.registration_id})"
+    
+
+class Classroom(models.Model):
+    """
+        Guarda as informacoes sobre a disciplina
+        
+        - subject: o nome da disciplina
+        - teacher: professor responsavel
+        - active_now: disciplina em atividade
+        
+    """
+    subject = models.CharField(max_length=100)
+    teacher = models.CharField(max_length=100)
+    active_now = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.subject
+
+
+class Attendance(models.Model):
+    """
+        Tabela transacional para conectar aluno a uma turma especifica
+        
+        student: id do aluno
+        classroom: disciplina vinculada
+        timestamp: data e hora do reconhecimento pelo totem
+        liveness_score: pontuacao do algoritmo de liveness
+        is_valid: resultado do liveness
+    """
+    
+    student = models.ForeignKey(Student, on_delete=models.CASCODE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCODE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    liveness_score = models.FloatField()
+    is_valid = models.BooleanField(default=False)
